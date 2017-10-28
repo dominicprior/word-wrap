023e214068f981b519a71cc597b62a0dd1bebc2c
Dominic Prior <dominicprior@yahoo.co.uk>
2017-10-28 18:59:27 +0100

    Get keypress just about working

--- wrap.hs
+++ wrap.hs
@@ -1,47 +1,67 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+--{-# LANGUAGE FlexibleContexts #-}
 
 import Data.IORef
 import qualified Graphics.UI.Threepenny as UI
-import qualified Graphics.UI.Threepenny.SVG  as SVG
-import Graphics.UI.Threepenny.Core
+import qualified Graphics.UI.Threepenny.SVG as SVG
+import           Graphics.UI.Threepenny.Core hiding (set, element, children)
+import qualified Graphics.UI.Threepenny.Core as THREE (set, children)
+import Control.Lens hiding ((#))
+import qualified Control.Lens as L ((#))
+import Data.List (intersperse)
+
+data Model = Model { _front :: [String], _back :: [String] } deriving Show
+makeLenses ''Model
+
 main = startGUI defaultConfig setup
 
 infixl 8 #++
 (#++) :: UI Element -> [Element] -> UI Element
-a #++ xs = a #+ map element xs
+a #++ xs = a #+ map UI.element xs
+
+infixl 8 ##
+(##) :: (MonadIO m, Widget w) => w -> (m Element -> b) -> b
+e ## f = UI.element e # f
+
+s = THREE.set
 
 setup :: Window -> UI ()
 setup w = do
-    r1 <- liftIO $ newIORef []
-    r2 <- liftIO $ newIORef []
-    elemCircle <- SVG.circle
-        # set SVG.cx "100" # set SVG.cy "2" # set SVG.r "2"
-        # set SVG.fill "blue"
-    context <- SVG.svg
-        # set SVG.width "300"
-        # set SVG.height "100" #++ [elemCircle]
-    out1  <- UI.span # set text ""
-    out2  <- UI.span # set text ""
-    bar <- UI.span # set text "|" # set style [("color", "red")]
-    sp <- UI.span #. "sp" #++ [out1, bar, out2]
-    wrap <- UI.div #. "wrap"
-        # set style [("width","300px"),("height","300px"),("border","solid black 1px"),
-                     ("position", "absolute")]
-        #++ [sp]
-    svgWrap <- UI.div #. "svgwrap"
-        # set style [("width","300px"),("height","300px"), ("position", "absolute")
-                      ]
-        #++ [context]
-    getBody w #++ [sp, svgWrap]
+    model <- liftIO $ newIORef $ Model [""] [""]
     b <- getBody w
 
-    on UI.keydown b $ fdown r1 r2 out1 out2 elemCircle
-    on my_keypress b $ fpress r1 out1 elemCircle
+    --on UI.keydown b $ fdown model out1 out2
+    on my_keypress b $ fpress model b
 
-fdown :: IORef [Char] -> IORef [Char] -> Element -> Element -> Element -> Int -> UI Element
-fdown r1 r2 el1 el2 circ k = do
-  a <- liftIO $ readIORef r1
-  b <- liftIO $ readIORef r2
+fpress :: IORef Model -> Element -> Char -> UI Element
+fpress model b c = do
+  m <- liftIO $ readIORef model
+  let m' = over front (appendChar c) m
+  liftIO $ writeIORef model m'
+  draw m' b
+
+appendChar :: Char -> [String] -> [String]
+appendChar ' ' ss = ss ++ [""]
+appendChar c   ss = init ss ++ [last ss ++ [c]]
+
+draw :: Model -> Element -> UI Element
+draw m b = do
+  ch <- sequence $
+      (intersperse spacer $ map string $ m ^. front) ++ [bar]
+  b ## s THREE.children ch
+
+spacer :: UI Element
+spacer = UI.span # s style [("padding-left", "6px")]
+
+bar :: UI Element
+bar = string "|" # s style [("color", "red")]
+
+fdown :: IORef Model -> Element -> Element -> Int -> UI Element
+fdown model el1 el2 k = do
+  m <- liftIO $ readIORef model
+  let a = "foo"
+  let b = "foo"
   let (a', b') =
         case k of
           8 ->  if null a then (a,b) else (init a, b)  -- backspace
@@ -51,24 +71,9 @@ fdown r1 r2 el1 el2 circ k = do
           39 -> if null b then (a,b) else (a ++ [head b], tail b)  -- right arrow
           46 -> if null b then (a,b) else (a, tail b)  -- delete
           _ -> (a, b)
-  liftIO $ writeIORef r1 a'
-  liftIO $ writeIORef r2 b'
-  element el1 # set text a'
-  element el2 # set text b'
-  moveBlob circ
-
-fpress :: IORef [Char] -> Element -> Element -> Char -> UI Element
-fpress r el circ c = do
-  a <- liftIO $ readIORef r
-  let a' = a ++ [c]
-  liftIO $ writeIORef r a'
-  element el # set text a'
-  moveBlob circ
-
-moveBlob :: Element -> UI Element
-moveBlob circ = do
-  (k :: Double) <- callFunction $ ffi "$('.sp').width()"
-  element circ # set SVG.cx (show $ k)
+  liftIO $ writeIORef model m
+  el1 ## s text a'
+  el2 ## s text b'
 
 -- | Key pressed while element has focus.
 my_keypress :: Element -> Event Char
