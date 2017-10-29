c0c53d94e2075dd75edc64fabfcea43a47b01ca4
Dominic Prior <dominicprior@yahoo.co.uk>
2017-10-29 10:35:00 +0000

    Return to using strings rather than tokens

--- wrap.hs
+++ wrap.hs
@@ -10,8 +10,9 @@ import qualified Graphics.UI.Threepenny.Core as THREE (set, children)
 import Control.Lens hiding ((#))
 import qualified Control.Lens as L ((#))
 import Data.List (intersperse)
+import Data.List.Split (splitOn)
 
-data Model = Model { _front :: [String], _back :: [String] } deriving Show
+data Model = Model { _front :: String, _back :: String } deriving Show
 makeLenses ''Model
 
 main = startGUI defaultConfig setup
@@ -28,7 +29,7 @@ s = THREE.set
 
 setup :: Window -> UI ()
 setup w = do
-    model <- liftIO $ newIORef $ Model [""] [""]
+    model <- liftIO $ newIORef $ Model "" ""
     b <- getBody w
     on UI.keydown b  $ fdown  model b
     on my_keypress b $ fpress model b
@@ -36,18 +37,16 @@ setup w = do
 fpress :: IORef Model -> Element -> Char -> UI Element
 fpress model b c = do
   m <- liftIO $ readIORef model
-  let m' = over front (appendChar c) m
+  let m' = over front (++ [c]) m
   liftIO $ writeIORef model m'
   draw m' b
-
-appendChar :: Char -> [String] -> [String]
-appendChar ' ' ss = ss ++ [""]
-appendChar c   ss = init ss ++ [last ss ++ [c]]
 
 draw :: Model -> Element -> UI Element
 draw m b = do
   ch <- sequence $
-           (strings $ m ^. front) ++ [bar] ++ (strings $ m ^. back)
+           (strings $ splitOn " " $ m ^. front) ++
+           [bar] ++
+           (strings $ splitOn " " $ m ^. back)
   b ## s THREE.children ch
 
 strings :: [String] -> [UI Element]
@@ -66,9 +65,9 @@ fdown model body k = do
   let b = m ^. back
   let (a', b') =
         case k of
-          8 ->  (deleteLast a, b)  -- backspace
-          36 -> ([""], a ++ b)  -- home  -- http://keycode.info/
-          35 -> (a ++ b, [""])  -- end
+          8 ->  if null a then (a,b) else (init a, b)  -- backspace
+          36 -> ([], a ++ b)  -- home  -- http://keycode.info/
+          35 -> (a ++ b, [])  -- end
           37 -> if null a then (a,b) else (init a, last a : b)  -- left arrow
           39 -> if null b then (a,b) else (a ++ [head b], tail b)  -- right arrow
           46 -> if null b then (a,b) else (a, tail b)  -- delete
@@ -76,18 +75,6 @@ fdown model body k = do
   let m' = Model a' b'
   liftIO $ writeIORef model m'
   draw m' body
-
-deleteFirst :: [String] -> [String]
-deleteFirst [""] = [""]
-deleteFirst ("":ss) = ss
-deleteFirst ((c:cs):ss) = cs:ss
-
-deleteLast :: [String] -> [String]
-deleteLast = revrev . deleteFirst . revrev
-
-revrev :: [[a]] -> [[a]]
-revrev = reverse . map reverse
-
 
 -- | Key pressed while element has focus.
 my_keypress :: Element -> Event Char
