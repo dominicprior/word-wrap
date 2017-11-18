c5f82404cf221cd7bc4a3f80a3e54f799c97c79b
Dominic Prior <dominicprior@yahoo.co.uk>
2017-11-18 12:43:53 +0000

    Tidy

--- wrap.hs
+++ wrap.hs
@@ -6,7 +6,7 @@ import Data.IORef
 import qualified Graphics.UI.Threepenny as UI
 import qualified Graphics.UI.Threepenny.SVG as SVG
 import           Graphics.UI.Threepenny.Core hiding (set, element, children)
-import qualified Graphics.UI.Threepenny.Core as THREE (set, children)
+import qualified Graphics.UI.Threepenny.Core as T (set, children)
 import Control.Lens hiding ((#))
 import qualified Control.Lens as L ((#))
 import Data.List (intersperse)
@@ -16,16 +16,6 @@ data Model = Model { _front :: String, _back :: String } deriving Show
 makeLenses ''Model
 
 main = startGUI defaultConfig setup
-
-infixl 8 #++
-(#++) :: UI Element -> [Element] -> UI Element
-a #++ xs = a #+ map UI.element xs
-
-infixl 8 ##
-(##) :: (MonadIO m, Widget w) => w -> (m Element -> b) -> b
-e ## f = UI.element e # f
-
-s = THREE.set
 
 setup :: Window -> UI ()
 setup w = do
@@ -43,20 +33,17 @@ fpress model b c = do
 
 draw :: Model -> Element -> UI Element
 draw m b = do
-  ch <- sequence $
-           (strings $ splitOn " " $ m ^. front) ++
-           [bar] ++
-           (strings $ splitOn " " $ m ^. back)
-  b ## s THREE.children ch
+  ch <- sequence $ sp (m ^. front) ++ [bar] ++ sp (m ^. back)
+  UI.element b # T.set T.children ch
 
-strings :: [String] -> [UI Element]
-strings ss = intersperse spacer $ map string ss
+sp :: String -> [UI Element]
+sp = intersperse spacer . map string . splitOn " "
 
 spacer :: UI Element
-spacer = UI.span # s style [("padding-left", "6px")]
+spacer = UI.span # T.set style [("padding-left", "6px")]
 
 bar :: UI Element
-bar = string "|" # s style [("color", "red")]
+bar = string "|" # T.set style [("color", "red")]
 
 fdown :: IORef Model -> Element -> Int -> UI Element
 fdown model body k = do
